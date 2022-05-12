faadf0b2b2985ae5561d87e6415e2db08f77bb89
guilinhd <394811364@qq.com>
2022-05-12 18:06:17 +0800

    调整呼吸服务到新的文件夹

---
+++ src/SleepMonitor/BreathServices/WaveBreathService.cs
@@ -0,0 +1,35 @@
+﻿using SleepMonitor.IServices;
+using SleepMonitor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepMonitor.Services
+{
+    /// <summary>
+    /// 生成峰谷差数据
+    /// </summary>
+    public class WaveBreathService : Queue<SensorRawModel>, IBaseService<DifferenceService>
+    {
+        public WaveBreathService(int count)
+        {
+            FilterCount = count;
+        }
+
+        public int FilterCount { set; get; }
+
+        public bool Filter(DifferenceService service)
+        {
+            if (Count > FilterCount)
+            {
+                service.Enqueue(this.ElementAt(1).X - this.ElementAt(0).X);
+                Dequeue();
+                return true;
+            }
+           
+            return false;
+        }
+    }
+}
