4258505b414df70e38c1858483e4c83f432ab5e6
guilinhd <394811364@qq.com>
2022-05-12 17:40:14 +0800

    完善呼吸监测服务

---
+++ src/SleepMonitor/Services/WaveBreathService.cs
@@ -0,0 +1,32 @@
+﻿using SleepMonitor.IServices;
+using SleepMonitor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepMonitor.Services
+{
+    public class WaveBreathService : Queue<SensorRawModel>, IBaseService<DifferenceService>
+    {
+        public WaveBreathService(int count)
+        {
+            FilterCount = count;
+        }
+
+        public int FilterCount { set; get; }
+
+        public bool Filter(DifferenceService service)
+        {
+            if (Count < FilterCount)
+            {
+                return false;
+            }
+
+            service.Enqueue(this.ElementAt(1).X - this.ElementAt(0).X);
+
+            return true;
+        }
+    }
+}
