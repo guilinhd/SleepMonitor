4e640129ef838871804b3e84e8da23dd1dafe8d2
guilinhd <394811364@qq.com>
2022-05-13 16:10:29 +0800

    新增峰谷差服务

---
+++ src/SleepService/BreathServices/DifferenceService.cs
@@ -0,0 +1,16 @@
+﻿using SleepService.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepService.BreathServices
+{
+    public class DifferenceService : BaseService
+    {
+        public DifferenceService(int count) : base(count)
+        {
+        }
+    }
+}
