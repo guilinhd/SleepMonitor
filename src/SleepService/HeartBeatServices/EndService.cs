e9491e9f031543cb5b8103b56b8d29d2d62d213e
guilinhd <394811364@qq.com>
2022-05-14 20:39:55 +0800

    规范变量名

--- src/SleepService/HeartBeatServices/EndService.cs
+++ src/SleepService/HeartBeatServices/EndService.cs
@@ -23,40 +23,40 @@ namespace SleepService.HeartBeatServices
                 //峰谷差
                 double[] dots = this.Select(f => f.Y).ToArray();
                 double average = 0.5 * (dots.Max() - dots.Min());
-                //波峰                //波谷     
-                WaveModel raw = Dequeue(); WaveModel peak; WaveModel trough;
-                if (!raw.Type)      //当前波形是波谷
+                                            //波峰              //波谷     
+                WaveModel wave = Dequeue(); WaveModel peakWave; WaveModel troughWave;
+                if (!wave.Type)      //当前波形是波谷
                 {
-                    trough = raw;  //直接设置波谷
+                    troughWave = wave;  //直接设置波谷
                 }
                 else
                 {
-                    trough = Dequeue();   //设置下一个波形是波谷
+                    troughWave = Dequeue();   //设置下一个波形是波谷
                 }
-                peak = Dequeue(); //设置波峰
+                peakWave = Dequeue(); //设置波峰
 
                 Datas.Clear();
 
                 #region 仰睡状态
-                double ssfgc = peak.Y - this.ElementAt(0).Y;//仰睡状态的实时峰谷差
+                double ssfgc = peakWave.Y - this.ElementAt(0).Y;//仰睡状态的实时峰谷差
                 double ssfgcmf = this.ElementAt(1).Y - this.ElementAt(2).Y;//仰睡数列内头部两个元素的峰谷差
                 double ssfgcmi = this.ElementAt(3).Y - this.ElementAt(4).Y;
                 
                 //当前波峰后面3个波峰是否是连续递减
                 if (ssfgc > average && ssfgc > ssfgcmf && ssfgc > ssfgcmi)
                 {
-                    Datas.Add("心跳正向波", peak.X);
+                    Datas.Add("心跳正向波", peakWave.X);
                 }
                 #endregion
 
                 #region 侧睡状态
-                double csfgc = Math.Abs(peak.Y - trough.Y);//侧睡状态的实时峰谷差
+                double csfgc = Math.Abs(peakWave.Y - troughWave.Y);//侧睡状态的实时峰谷差
                 double csfgcmf = Math.Abs(this.ElementAt(1).Y - this.ElementAt(0).Y);//侧睡数列内头部两个元素的峰谷差
                 double csfgcmi = Math.Abs(this.ElementAt(3).Y - this.ElementAt(2).Y);
 
                 if (csfgc > average && csfgc > csfgcmf && csfgc > csfgcmi)
                 {
-                    Datas.Add("心跳反向波", trough.X);
+                    Datas.Add("心跳反向波", troughWave.X);
                 }
                 #endregion
 
