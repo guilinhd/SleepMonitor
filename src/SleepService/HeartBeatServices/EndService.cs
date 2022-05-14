b602e11213d8c73029dd63ed8416825be7ffb32b
guilinhd <394811364@qq.com>
2022-05-14 11:26:14 +0800

    完成心跳检测服务对原始数据分析功能

---
+++ src/SleepService/HeartBeatServices/EndService.cs
@@ -0,0 +1,69 @@
+﻿using SleepService.Models;
+using SleepService.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepService.HeartBeatServices
+{
+    public class EndService : BaseService
+    {
+        public EndService(int count) : base(count)
+        {
+        }
+
+        public override bool Filter()
+        {
+            Func = () => true;
+
+            if (base.Filter())
+            {
+                //峰谷差
+                double[] dots = this.Select(f => f.Y).ToArray();
+                double average = 0.5 * (dots.Max() - dots.Min());
+                //波峰                //波谷     
+                WaveModel raw = Dequeue(); WaveModel peak; WaveModel trough;
+                if (!raw.Type)      //当前波形是波谷
+                {
+                    trough = raw;  //直接设置波谷
+                }
+                else
+                {
+                    trough = Dequeue();   //设置下一个波形是波谷
+                }
+                peak = Dequeue(); //设置波峰
+
+                Datas.Clear();
+
+                #region 仰睡状态
+                double ssfgc = peak.Y - this.ElementAt(0).Y;//仰睡状态的实时峰谷差
+                double ssfgcmf = this.ElementAt(1).Y - this.ElementAt(2).Y;//仰睡数列内头部两个元素的峰谷差
+                double ssfgcmi = this.ElementAt(3).Y - this.ElementAt(4).Y;
+                
+                //当前波峰后面3个波峰是否是连续递减
+                if (ssfgc > average && ssfgc > ssfgcmf && ssfgc > ssfgcmi)
+                {
+                    Datas.Add("正向波", peak.X);
+                }
+                #endregion
+
+                #region 侧睡状态
+                double csfgc = Math.Abs(peak.Y - trough.Y);//侧睡状态的实时峰谷差
+                double csfgcmf = Math.Abs(_raws.ElementAt(1).Y - _raws.ElementAt(0).Y);//侧睡数列内头部两个元素的峰谷差
+                double csfgcmi = Math.Abs(_raws.ElementAt(3).Y - _raws.ElementAt(2).Y);
+
+                if (csfgc > average && csfgc > csfgcmf && csfgc > csfgcmi)
+                {
+                    Datas.Add("反向波", trough.X);
+                }
+                #endregion
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/SleepService/HeartBeatServices/FilteringService.cs
@@ -0,0 +1,62 @@
+﻿using SleepService.Models;
+using SleepService.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepService.HeartBeatServices
+{
+    public class FilteringService : BaseService
+    {
+        public FilteringService(int count) : base(count)
+        {
+        }
+
+        public override bool Filter()
+        {
+            var next = GetNext();
+            WaveModel wave = new WaveModel();
+            Func = () =>
+            {
+                //左边3个数据的平均值
+                double leftValue = (this.ElementAt(0).Y + this.ElementAt(1).Y + this.ElementAt(2).Y) / 3;
+                //后边3个数据的平均值
+                double RightValue = (this.ElementAt(4).Y + this.ElementAt(5).Y + this.ElementAt(6).Y) / 3;
+                //中间第3个值
+                double midValue = this.ElementAt(3).Y;
+
+                //最后一个波形的类型
+                bool waveType = false;
+                if (next.Count > 0)
+                {
+                    waveType = !next.Last().Type;
+                }
+                int waveStatus = 0;
+                if (leftValue < midValue && RightValue < midValue && waveType)  //强制寻波峰
+                {
+                    waveStatus = 1;
+                }
+                else if (leftValue > midValue && RightValue > midValue && !waveType) //强制寻波谷
+                {
+                    waveStatus = 2;
+                }
+
+                if (waveStatus > 0)
+                {
+                    wave.Type = waveStatus == 1;
+                    wave.X = TotalCount;
+                    wave.Y = midValue;
+
+                    Console.WriteLine($"心跳有效波形数:{next.Count + 1}, 心跳波形X的值:{TotalCount}, 当前心跳波形计数器:{WaveCount}");
+                    return true;
+                }
+
+                return false;
+            };
+
+            return base.Filter();
+        }
+    }
+}
